d69979b698850d82cde0ab0a8d290bdcea116fe7
Rastislav Lamos <lamos.rasto@gmail.com>
2016-08-22 12:22:27 +0200

    Move features to ProductPurchaseFeaturesList

--- client/blocks/product-purchase-features/index.jsx
+++ client/blocks/product-purchase-features/index.jsx
@@ -3,7 +3,6 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { localize } from 'i18n-calypso';
-import { find } from 'lodash';
 import classNames from 'classnames';
 
 /**
@@ -14,33 +13,11 @@ import {
 	PLAN_PERSONAL,
 	PLAN_PREMIUM,
 	PLAN_BUSINESS,
-	PLAN_JETPACK_FREE,
-	PLAN_JETPACK_PREMIUM,
-	PLAN_JETPACK_BUSINESS,
-	PLAN_JETPACK_PREMIUM_MONTHLY,
-	PLAN_JETPACK_BUSINESS_MONTHLY
 } from 'lib/plans/constants';
-import FindNewTheme from './find-new-theme';
-import AdvertisingRemoved from './advertising-removed';
-import GoogleVouchers from './google-vouchers';
-import CustomizeTheme from './customize-theme';
-import VideoAudioPosts from './video-audio-posts';
-import MonetizeSite from './monetize-site';
-import CustomDomain from './custom-domain';
-import GoogleAnalyticsStats from './google-analytics-stats';
 import HappinessSupport from './happiness-support';
-import JetpackAntiSpam from './jetpack-anti-spam';
-import JetpackBackupSecurity from './jetpack-backup-security';
-import JetpackReturnToDashboard from './jetpack-return-to-dashboard';
-import JetpackSurveysPolls from './jetpack-surveys-polls';
-import JetpackWordPressCom from './jetpack-wordpress-com';
 import CurrentPlanHeader from './current-plan-header';
-import {
-	isPremium as isWpcomPremium,
-	isBusiness as isWpcomBusiness
-} from 'lib/products-values';
-import { isWordadsInstantActivationEligible } from 'lib/ads/utils';
 import { plansList } from 'lib/plans/constants';
+import ProductPurchaseFeaturesList from './product-purchase-features-list';
 
 class PlanPurchaseFeatures extends Component {
 	static propTypes = {
@@ -58,200 +35,6 @@ class PlanPurchaseFeatures extends Component {
 	static defaultProps = {
 		isPlaceholder: false
 	};
-
-	getBusinessFeatures() {
-		const {
-			selectedSite,
-			sitePlans,
-		} = this.props;
-
-		const plan = find( sitePlans.data, isWpcomBusiness );
-
-		return [
-			<CustomDomain
-				selectedSite={ selectedSite }
-				hasDomainCredit={ plan && plan.hasDomainCredit }
-				key="customDomainFeature"
-			/>,
-			<AdvertisingRemoved
-				isBusinessPlan
-				key="advertisingRemovedFeature"
-			/>,
-			<GoogleVouchers
-				selectedSite={ selectedSite }
-				key="googleVouchersFeature"
-			/>,
-			<CustomizeTheme
-				selectedSite={ selectedSite }
-				key="customizeThemeFeature"
-			/>,
-			<VideoAudioPosts
-				selectedSite={ selectedSite }
-				key="videoAudioPostsFeature"
-			/>,
-			<GoogleAnalyticsStats
-				selectedSite={ selectedSite }
-				key="googleAnalyticsStatsFeature"
-			/>,
-			<FindNewTheme
-				selectedSite={ selectedSite }
-				key="findNewThemeFeature"
-			/>,
-			isWordadsInstantActivationEligible( selectedSite )
-				? <MonetizeSite
-					selectedSite={ selectedSite }
-					key="monetizeSiteFeature"
-				/>
-				: null
-		];
-	}
-
-	getPremiumFeatures() {
-		const {
-			selectedSite,
-			sitePlans,
-		} = this.props;
-
-		const plan = find( sitePlans.data, isWpcomPremium );
-
-		return [
-			<CustomDomain
-				selectedSite={ selectedSite }
-				hasDomainCredit={ plan && plan.hasDomainCredit }
-				key="customDomainFeature"
-			/>,
-			<AdvertisingRemoved
-				isBusinessPlan={ false }
-				key="advertisingRemovedFeature"
-			/>,
-			<GoogleVouchers
-				selectedSite={ selectedSite }
-				key="googleVouchersFeature"
-			/>,
-			<CustomizeTheme
-				selectedSite={ selectedSite }
-				key="customizeThemeFeature"
-			/>,
-			<VideoAudioPosts
-				selectedSite={ selectedSite }
-				key="videoAudioPostsFeature"
-			/>,
-			isWordadsInstantActivationEligible( selectedSite )
-				? <MonetizeSite
-					selectedSite={ selectedSite }
-					key="monetizeSiteFeature"
-				/>
-				: null
-		];
-	}
-
-	getPersonalFeatures() {
-		const {
-			selectedSite,
-			sitePlans,
-		} = this.props;
-
-		const plan = find( sitePlans.data, isWpcomPremium );
-
-		return [
-			<CustomDomain
-				selectedSite={ selectedSite }
-				hasDomainCredit={ plan && plan.hasDomainCredit }
-				key="customDomainFeature"
-			/>,
-			<AdvertisingRemoved
-				isBusinessPlan={ false }
-				key="advertisingRemovedFeature"
-			/>
-		];
-	}
-
-	getJetpackFreeFeatures() {
-		const {	selectedSite } = this.props;
-
-		return [
-			<JetpackWordPressCom
-				selectedSite={ selectedSite }
-				key="jetpackWordPressCom"
-			/>,
-			<JetpackReturnToDashboard
-				selectedSite={ selectedSite }
-				key="jetpackReturnToDashboard"
-			/>
-		];
-	}
-
-	getJetpackPremiumFeatures() {
-		const {	selectedSite } = this.props;
-
-		return [
-			<JetpackBackupSecurity
-				key="jetpackBackupSecurity"
-			/>,
-			<JetpackAntiSpam
-				key="jetpackAntiSpam"
-			/>,
-			<JetpackWordPressCom
-				selectedSite={ selectedSite }
-				key="jetpackWordPressCom"
-			/>,
-			<JetpackReturnToDashboard
-				selectedSite={ selectedSite }
-				key="jetpackReturnToDashboard"
-			/>
-		];
-	}
-
-	getJetpackBusinessFeatures() {
-		const {	selectedSite } = this.props;
-
-		return [
-			<JetpackBackupSecurity
-				key="jetpackBackupSecurity"
-			/>,
-			<JetpackAntiSpam
-				key="jetpackAntiSpam"
-			/>,
-			<JetpackSurveysPolls
-				key="jetpackSurveysPolls"
-			/>,
-			<JetpackWordPressCom
-				selectedSite={ selectedSite }
-				key="jetpackWordPressCom"
-			/>,
-			<JetpackReturnToDashboard
-				selectedSite={ selectedSite }
-				key="jetpackReturnToDashboard"
-			/>
-		];
-	}
-
-	getPlanPurchaseFeatures() {
-		const { plan, isPlaceholder } = this.props;
-
-		if ( isPlaceholder ) {
-			return null;
-		}
-
-		switch ( plan ) {
-			case PLAN_BUSINESS:
-				return this.getBusinessFeatures();
-			case PLAN_PREMIUM:
-				return this.getPremiumFeatures();
-			case PLAN_PERSONAL:
-				return this.getPersonalFeatures();
-			case PLAN_JETPACK_FREE:
-				return this.getJetpackFreeFeatures();
-			case PLAN_JETPACK_PREMIUM:
-			case PLAN_JETPACK_PREMIUM_MONTHLY:
-				return this.getJetpackPremiumFeatures();
-			case PLAN_JETPACK_BUSINESS:
-			case PLAN_JETPACK_BUSINESS_MONTHLY:
-				return this.getJetpackBusinessFeatures();
-			default:
-				return null;
-		}
-	}
 
 	getPlanHeaderWording() {
 		const { plan, translate } = this.props;
@@ -304,7 +87,12 @@ class PlanPurchaseFeatures extends Component {
 	}
 
 	render() {
-		const { isPlaceholder } = this.props;
+		const {
+			plan,
+			selectedSite,
+			sitePlans,
+			isPlaceholder
+		} = this.props;
 
 		const headerClasses = classNames( 'product-purchase-features__header', {
 			'is-placeholder': isPlaceholder
@@ -315,9 +103,12 @@ class PlanPurchaseFeatures extends Component {
 				<div className={ headerClasses }>
 					{ this.getPlanPurchaseHeader() }
 				</div>
-				<div className="product-purchase-features__list">
-					{ this.getPlanPurchaseFeatures() }
-				</div>
+				<ProductPurchaseFeaturesList
+					plan={ plan }
+					selectedSite={ selectedSite }
+					sitePlans={ sitePlans }
+					isPlaceholder={ isPlaceholder }
+				/>
 			</div>
 		);
 	}
